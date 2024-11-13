0a48331be8dfc3cdeaf19b4c11b8acfc51af0789
hilaryarale <hilarycibertec@gmail.com>
2024-11-13 14:26:20 -0500

    cambio

---
+++ src/main/java/com/example/demo/PidBackGrupo03Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PidBackGrupo03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PidBackGrupo03Application.class, args);
+	}
+
+}
